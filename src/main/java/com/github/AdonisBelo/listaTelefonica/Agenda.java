fbad20a8067b0ab58e143bada60aa87eb47ca877
Danielgol <danielgol@yahoo.com.br>
2017-05-13 18:38:09 -0300

    Adicionando Construtor e Métodos Agenda

--- src/main/java/com/github/AdonisBelo/listaTelefonica/Agenda.java
+++ src/main/java/com/github/AdonisBelo/listaTelefonica/Agenda.java
@@ -1,10 +1,31 @@
 package com.github.AdonisBelo.listaTelefonica;
 
+import java.util.ArrayList;
+
 public class Agenda {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	ArrayList<Contato> contatos;
+	
+	Agenda(){
+		this.contatos = new ArrayList<Contato>();
+	}
+	
+	public void inserir(Contato c){
+		this.contatos.add(c);
+	}
+	
+	public ArrayList<Contato> buscarContato(String nome){
+		
+		ArrayList<Contato> resultado = new ArrayList<Contato>();
+		
+		for(int i=0;i < this.contatos.size();i++){
+			if(nome.equals(this.contatos.get(i).nome)){
+				resultado.add(this.contatos.get(i));
+			}
+		}
+		
+		return resultado;
+		
 	}
 
 }
