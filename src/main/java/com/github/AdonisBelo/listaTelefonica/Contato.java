194b7bdecc9680ce70eebcb9ebdbb66a4462a0bb
@AdonisBelo <adonisb2012@gmail.com>
2017-05-14 19:38:54 -0300

    Adicionando a classe Contato

--- src/main/java/com/github/AdonisBelo/listaTelefonica/Contato.java
+++ src/main/java/com/github/AdonisBelo/listaTelefonica/Contato.java
@@ -1,11 +1,42 @@
 package com.github.AdonisBelo.listaTelefonica;
 
+import java.util.ArrayList;
+
 public class Contato {
 
 	String nome;
-	//Telefone [] telefones;
-	String [] emails;
+	ArrayList<Telefone> telefones;
+
+	Contato(String nome){
+		this.nome = nome;
+		this.telefones = new ArrayList<Telefone>();
+	}
+
+	public void mostrarContato(int i){
+
+		System.out.println("-----------------------------------------");
+		System.out.println("("+(i+1)+").NOME: "+this.getNome());
+
+		for(int x=0; x<this.getTelefones().size();x++){
+			this.getTelefones().get(x).mostrarTelefone(x);
+		}
+		System.out.println("-----------------------------------------");
+	}
 	
-	//COMENTARIO
-	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public ArrayList<Telefone> getTelefones() {
+		return telefones;
+	}
+
+	public void setTelefones(ArrayList<Telefone> telefones) {
+		this.telefones = telefones;
+	}
+
 }
--- src/main/java/com/github/AdonisBelo/listaTelefonica/Usuario.java
+++
@@ -1,8 +0,0 @@
-package com.github.AdonisBelo.listaTelefonica;
-
-public class Usuario {
-	
-	//Agenda agenda;
-	
-
-}
